d8abfa501084e6228e3194231df0b417ac2cf5d7
Rameesllo <153506108+Rameesllo@users.noreply.github.com>
2026-06-22 11:22:47 +0530

    Remove build configuration from vite.config.js
    Removed custom build configuration from Vite config.

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-
- build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        second: resolve(__dirname, '2ndpage.html'),
-      },
-    },
-  },
 })
